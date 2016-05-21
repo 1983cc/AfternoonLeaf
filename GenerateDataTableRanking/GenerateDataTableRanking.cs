719f268922ac9360253b1a9918b3cce615693d43
bo cao <bo cao>
2016-05-21 14:58:37 +0800

    add

---
+++ GenerateDataTableRanking/GenerateDataTableRanking.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace AfternoonLeaf.Framework.Util
+{
+    public class DataTableUtil
+    {
+        public void GenerateDataTableRanking(ref DataTable toOperateDataTable, string sortByField, string rankingField)
+        {
+            // group the data elements
+            List<RankingGroup> rgc = new List<RankingGroup>();
+            foreach (DataRow dr1 in toOperateDataTable.Rows)
+            {
+                RankingGroup rg = new RankingGroup();
+                foreach (DataRow dr2 in toOperateDataTable.Rows)
+                {
+                    
+                    if (dr1 != dr2 && dr2 != null)
+                    {
+                        if (!rg.Indecies.Contains(toOperateDataTable.Rows.IndexOf(dr1)))
+                        {
+                            rg.Indecies.Add(toOperateDataTable.Rows.IndexOf(dr1));
+                            rg.Score = float.Parse((string)dr1[sortByField]);
+                        }
+
+                        if ((float.Parse((string)dr1[sortByField]) == float.Parse((string)dr2[sortByField]) && !rg.Indecies.Contains(toOperateDataTable.Rows.IndexOf(dr2))))
+                        {
+                            rg.Indecies.Add(toOperateDataTable.Rows.IndexOf(dr2));
+                            rg.Score = float.Parse((string)dr2[sortByField]);
+                        }                        
+                    }
+                    
+                }
+                if (!ExistsIndexcies(rgc, rg))
+                    rgc.Add(rg);
+            }
+
+            List<RankingGroup> sortedRgc = rgc.OrderByDescending(t => t.Score).ToList();
+
+            int ranking = 1;
+            foreach (RankingGroup rg in sortedRgc)
+            {
+                foreach (int i in rg.Indecies)
+                {
+                    toOperateDataTable.Rows[i][rankingField] = ranking;
+                }
+                ranking++;
+            }            
+        }
+
+        bool ExistsIndexcies(List<RankingGroup>rgc, RankingGroup rg)
+        {
+            foreach (RankingGroup trg in rgc)
+            {
+                foreach (int i in rg.Indecies)
+                {
+                    if (trg.Indecies.Any(t => t == i))
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        internal class RankingGroup
+        {
+            // string sortByFiled;
+            float score;
+            List<int> indecies = new List<int>();
+            int ranking;
+
+            public float Score
+            {
+                get { return score; }
+                set { score = value; }
+            }
+
+            public List<int> Indecies
+            {
+                get { return indecies; }
+                set { indecies = value; }
+            }
+
+            public int Ranking
+            {
+                get { return ranking; }
+                set { ranking = value; }
+            }
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,48 @@
+﻿using AfternoonLeaf.Framework.Util;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable testTable = new DataTable();
+            testTable.Columns.Add("Name");
+            testTable.Columns.Add("Score");
+            testTable.Columns.Add("Ranking");
+
+            DataRow dr1 = testTable.NewRow();
+            dr1["Name"] = "张三";
+            dr1["Score"] = 0;
+            testTable.Rows.Add(dr1);
+
+            DataRow dr2 = testTable.NewRow();
+            dr2["Name"] = "李四";
+            dr2["Score"] = 3;
+            testTable.Rows.Add(dr2);
+
+            DataRow dr3 = testTable.NewRow();
+            dr3["Name"] = "王五";
+            dr3["Score"] = 3;
+            testTable.Rows.Add(dr3);
+
+            DataRow dr4 = testTable.NewRow();
+            dr4["Name"] = "赵六";
+            dr4["Score"] = 0;
+            testTable.Rows.Add(dr4);
+
+            DataRow dr5 = testTable.NewRow();
+            dr5["Name"] = "牛七";
+            dr5["Score"] = 0;
+            testTable.Rows.Add(dr5);
+
+            DataTableUtil u1 = new DataTableUtil();
+            u1.GenerateDataTableRanking(ref testTable, "Score", "Ranking");
+        }
+    }
+}
